4f829f0e393f23f9617c910a23fd469b2f2cb7e1
FGM75 <fgonzalezm75@gmail.com>
2024-05-16 12:37:23 +0200

    añadido cabecera, footer, y navHamburguesa

---
+++ src/componentes/Cabecera.js
@@ -0,0 +1,53 @@
+import { NavLink } from "react-router-dom";
+import { useContext, useState } from "react";
+import { AuthContext } from "../contextos/AuthContext";
+import { ListadoNavegador } from "./ListadoNavegador";
+export const Cabecera = () => {
+  const { logueado } = useContext(AuthContext);
+  const [showItems, setShowItems] = useState(false);
+  const showNavItems = () => {
+    setShowItems(!!!showItems);
+  };
+  return (
+    <>
+      <header className="cabecera bg-white py-2 position-relative">
+        <nav className="container">
+          <div className="row align-items-center">
+            <div className="col-2 col-sm-1">
+              <NavLink to="/inicio" activeClassName="actual">
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/aquashare-94e77.appspot.com/o/PNG%20Logo.png?alt=media&token=f4915287-ba05-4f07-a597-1f125727bfff"
+                  alt="Logo de la página"
+                  className="rounded-circle"
+                  width="65px"
+                  height="65px"
+                />
+              </NavLink>
+            </div>
+            <div className="d-block d-md-none col-10 col-sm-11 text-right">
+              <button className="btn-hamburguer btn p-0" onClick={showNavItems}>
+                <span className="nav-line-btn"></span>
+                <span className="nav-line-btn"></span>
+                <span className="nav-line-btn"></span>
+              </button>
+            </div>
+            <div
+              className={`navbar-items${
+                showItems ? " show " : " "
+              }col-5 d-md-none`}
+            >
+              <ListadoNavegador
+                logueado={logueado}
+                mobile={true}
+                showNavItems={showNavItems}
+              />
+            </div>
+            <div className="d-none d-md-block col-10 col-sm-11 text-right">
+              <ListadoNavegador logueado={logueado} mobile={false} />
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
